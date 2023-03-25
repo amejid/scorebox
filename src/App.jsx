0ea4b9a7aa648f50f4d4c84726bb416a1298404f
amejid <abdelmejid89@gmail.com>
2023-03-25 10:47:07 +0300

    Wireup everything in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import SideMenu from "./components/SideMenu.jsx";
+import MainSection from "./components/MainSection.jsx";
+import NotificationSection from "./components/NotificationSection.jsx";
+import { useState } from "react";
+
 const App = () => {
-  return <div>Hello</div>;
+  const [openLeft, setOpenLeft] = useState(true);
+  const [openRight, setOpenRight] = useState(true);
+
+  const openLeftHandler = () => {
+    setOpenLeft((prev) => !prev);
+  };
+
+  const openRightHandler = () => {
+    setOpenRight((prev) => !prev);
+  };
+
+  return (
+    <div className="flex">
+      {openLeft && <SideMenu />}
+      <MainSection
+        onOpenLeft={openLeftHandler}
+        onOpenRight={openRightHandler}
+      />
+      {openRight && <NotificationSection />}
+    </div>
+  );
 };
 
 export default App;
